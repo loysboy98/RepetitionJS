6ae32097b1bb4b6b2c5ab6a54beec2fbd7827233
loysboy@mac.com <loysboy98@gmail.com>
2025-04-21 15:28:50 +0500

    first commit

---
+++ Lesson_1/js/script.js
@@ -0,0 +1,35 @@
+console.log(123);
+
+let number = 123;
+const leftBorderWith = 1;
+
+console.log(leftBorderWith, number)
+
+const userName = "John";
+let userAge = 25;
+
+userAge = 24;
+
+console.log("Name: " + userName + ", Age: " + userAge)
+
+// 
+const obj = {
+  name: 'Alex',
+  age: 25,
+  isMarried: false
+};
+
+console.log(obj.isMarried)
+console.log(obj["name"]);
+
+
+let storeName = "Product-Market";
+const storeDescription = {
+    budget : 10000,
+    employees : ["Alex", "Den", "Max"],
+    products : {
+        Apple : 3200,
+        Banana : 4000
+    },
+    open : true
+};
---
+++ Lesson_2/js/script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// alert('Hello World');
+
+// const result = confirm('Are you here ?');
+// console.log(result)
+
+// const answer = prompt('Вам есть 18 ?', '18');
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('What`s your name ?', '');
+// answers[1] = prompt('What`s your mail ?', '');
+// answers[2] = prompt('What`s your age ?', '');
+
+// document.write(answers);
+
+// typeof можно узнать какой тип данных
+// const answers = [];
+// console.log(typeof(answers));
+
+// это работает только `` кавычками и ${}
+// const category = 'toys'
+// console.log(`https://someurl.com/${category}`);
+// console.log(`https://someurl.com/${category}/page/5`);
+
+// const user = "Ivan";
+// alert(`Привет, ${user}`)
+
+// const userName = prompt('What is your name?', '');
+// alert(`Hello, ${userName}`);
+
+// console.log('arr' + " - object");
+//+'5' добавление плюс делает тип число
+// console.log(5 + +'5');
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+// оператор который будет добавлять по одной или удалять
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+// Boolean ===
+// console.log(2*4 == 8);
+// оператор сравнение
+// console.log(2*4 === 8);
+// //
+
+
+// const pin = prompt('Your pin code?', '');
+// const userPin = 4444;
+// console.log(pin == userPin);
+
+
+//для математические решения + добавляются чтобы строка сделать числа
+// const a = +prompt('первое число', '');
+// const b = +prompt('второе число', '');
+// alert(`Ответ: ${a+b}`);
+// console.log(a+b);
+
+// оператор "&& и" типа и это правда и это правда
+// const isChecked = true,
+//       isClose = true;
+// console.log(isChecked && isClose);
+
+//---------------------------------------
+
